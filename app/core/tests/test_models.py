a25794019007c03de0deb73ac1c4c130fcb85b31
javvadigihub <j_srikanth@outlook.com>
2020-03-11 21:31:57 +0530

    Setting Docker and django

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,23 @@
+# # from django.test import TestCase
+# # from django.contrib.auth import get_user_model
+
+# # class ModelTests(TestCase):
+
+# #     def test_create_user_with_email_successful(self):
+# #         #Test Creating a new user with email is sucessful
+# #         email = 'j_srikanth@outlook.com'
+# #         password = 'TestPass123'
+# #         user = get_user_model().objects.create_user(
+# #             email=email,
+# #             password=password
+# #         )
+
+# #     self.assertEqual(user.email, email)
+# #     self.assertTrue(user.check_password(password))
+
+#     def test_new_user_email_normalized(self):
+#         # Test the email for a new user in normalized
+#         email = 'j_srikanth@outlook.com'
+#         user = get_user_model().objects.create_user(email, 'test123')
+
+#         self.assertEqual(user.email, email.lower())
